6ed7700a9bb3cca2cdb6be721bb1cfa2c3bfe1d7
GitHub Action <action@github.com>
2026-09-19 17:21:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/kp2048/blockbenchidea/MyBundle.kt
+++ src/main/kotlin/com/github/kp2048/blockbenchidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kp2048.blockbenchidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kp2048/blockbenchidea/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/kp2048/blockbenchidea/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.kp2048.blockbenchidea.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kp2048.blockbenchidea.MyBundle
+import com.github.kp2048.blockbenchidea.services.MyProjectService
 import javax.swing.JButton
 
 
